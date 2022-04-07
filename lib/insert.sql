415246622c9d6a5e7ee3f0fa81c966c592a4e398
Zachary Sain <zachsain@gmail.com>
2022-04-07 12:49:21 -0500

    commit tree 1fac1eb1b841db4103e4f7ff75fc661b19380e6f

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+-- CREATE TABLE bears (
+--   id INTEGER PRIMARY KEY,
+--   name TEXT,
+--   age INTEGER,
+--   sex TEXT,
+--   color TEXT,
+--   temperament TEXT,
+--   alive INTEGER
+-- );
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', 'Brown', 'Angry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 22, 'M', 'Black', 'Happy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 13, 'F', 'Red', 'Cool', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 20, 'M', 'Brown', 'Chill', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 12, 'F', 'Black', 'Angry', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 33, 'M', 'Brown', 'Mean', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 5, 'F', 'Brown', 'Fun', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 55, 'F', 'Black', 'Sad', 0);
+
+
+
+
+
